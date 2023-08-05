448381181b28e48410c0acfa1d726597c0132088
Taqaddus <taqaddusshafi9086@gmail.com>
2023-08-05 14:04:09 +0530

    Fixed search issue and removed the lables

--- src/component/card/Card.jsx
+++ src/component/card/Card.jsx
@@ -1,32 +1,19 @@
-
-import React from "react";
 import styles from "./Card.module.css";
 
-const Card = ({ cardData, title, labels }) => {
-  if(!cardData)return null;
+const Card = ({ cardData, title }) => {
+  if (!cardData) return null;
   return (
-    <div className={`${styles.card} ${styles.container}`}>
+    <div className={styles.container}>
       <div className={styles.title}>{title}</div>
-      <div className={styles.content}>
-        <div className={styles.row}>
-          <div className={styles.column}>
-            {labels.map((label) => (
-              <div key={label} className={styles.label}>
-                {label}
-              </div>
-            ))}
-          </div>
-          <div className={styles.column}>
-            {Object.entries(cardData).map(([label, value]) => (
-              <div key={label} className={styles.value}>
-                {value}
-              </div>
-            ))}
-          </div>
+      {Object.entries(cardData).map(([k, v]) => (
+        <div className={styles.cell}>
+          <div className={styles.value}>{k}</div>
+          <div className={styles.value}>{v}</div>
         </div>
-      </div>
+      ))}
     </div>
   );
 };
-
 export default Card;
+
+
